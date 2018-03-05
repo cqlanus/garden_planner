fd565e3c59a27683a6bece2daa6bbeba32e59404
Chris Lanus <cqlanus@gmail.com>
2018-03-04 21:31:48 -0600

    adding frost bars

--- app/components/UserHome/UserHome.js
+++ app/components/UserHome/UserHome.js
@@ -6,18 +6,26 @@ import {
     VictoryTheme,
     VictoryArea,
     VictoryLine,
-    VictoryZoomContainer
+    VictoryBar,
+    VictoryTooltip
 } from 'victory';
 import { getWeatherNorms } from '../../redux';
+import { getDayOfYear } from '../../utils/dates';
 
 type Props = {
     getWeatherNorms: string => void,
     plants: mixed,
+    station: Station
+};
+
+type Station = {
+    daily: {
+        minTemps: Array<number>,
+        maxTemps: Array<number>
+    },
     station: {
-        daily: {
-            minTemps: Array<number>,
-            maxTemps: Array<number>
-        }
+        last_frost_50: string,
+        first_frost_50: string
     }
 };
 
@@ -33,7 +41,7 @@ type Event = {
 /**
  * COMPONENT
  */
-export class UserHome extends Component<Props, State> {
+class UserHome extends Component<Props, State> {
     state = {
         zip: '60007'
     };
@@ -42,6 +50,12 @@ export class UserHome extends Component<Props, State> {
         evt.preventDefault();
         this.props.getWeatherNorms(this.state.zip);
     };
+
+    _createBarData = (frostDay: string, station: Station) => ({
+        x: new Date(2018, 0, getDayOfYear(frostDay)),
+        y: station.daily.maxTemps.filter(temp => temp > 0)[getDayOfYear(frostDay)],
+        label: frostDay
+    });
 
     _handleChange = (evt: Event) => this.setState({ zip: evt.target.value });
 
@@ -61,12 +75,11 @@ export class UserHome extends Component<Props, State> {
                     <VictoryChart
                         width={700}
                         height={500}
-                        animate
                         scale={{ x: 'time' }}
                         theme={VictoryTheme.material}
                     >
                         <VictoryArea
-                            style={{ data: { fill: 'lightsalmon' } }}
+                            style={{ data: { fill: 'mistyrose' } }}
                             x={'day'}
                             y={'temp'}
                             data={station.daily.maxTemps
@@ -77,7 +90,7 @@ export class UserHome extends Component<Props, State> {
                                 }))}
                         />
                         <VictoryArea
-                            style={{ data: { fill: 'lightblue' } }}
+                            style={{ data: { fill: 'lightcyan' } }}
                             x={'day'}
                             y={'temp'}
                             data={station.daily.minTemps
@@ -88,6 +101,13 @@ export class UserHome extends Component<Props, State> {
                                 }))}
                         />
                         <VictoryLine
+                            style={{
+                                data: {
+                                    stroke: 'steelblue',
+                                    strokeDasharray: [2, 10],
+                                    strokeWidth: 3
+                                }
+                            }}
                             x={'day'}
                             y={'temp'}
                             data={station.daily.minTemps
@@ -97,6 +117,18 @@ export class UserHome extends Component<Props, State> {
                                     temp: 32
                                 }))}
                         />
+
+                        {station.station.last_frost_50 !== '-666' &&
+                            station.station.first_frost_50 !== '-666' && (
+                                <VictoryBar
+                                    data={[
+                                        this._createBarData(station.station.last_frost_50, station),
+                                        this._createBarData(station.station.first_frost_50, station),
+                                    ]}
+                                    style={{data: { width: 3, fill: 'steelblue' }}}
+                                    labelComponent={<VictoryTooltip />}
+                                />
+                            )}
                     </VictoryChart>
                 )}
             </div>
---
+++ app/utils/dates.js
@@ -0,0 +1,9 @@
+// @flow
+export const getDayOfYear = (dateStr: string) => {
+    const [ month, day ] = dateStr.split('/');
+    const now = new Date(2018, +month - 1, +day);
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+};
